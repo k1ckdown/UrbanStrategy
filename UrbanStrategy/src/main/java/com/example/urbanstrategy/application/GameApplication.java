43003e865c67be4319d8ae0027b6b693ea64ca35
Ivan Semenov <DevSemTsk@gmail.com>
2023-06-06 18:56:11 +0700

    updated game app

--- UrbanStrategy/src/main/java/com/example/urbanstrategy/application/GameApplication.java
+++ UrbanStrategy/src/main/java/com/example/urbanstrategy/application/GameApplication.java
@@ -9,21 +9,22 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public final class GameApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
         final GameView gameAnchorPane = new GameView();
-        final GamePresenter gameController = new GamePresenter(gameAnchorPane);
-        gameAnchorPane.setPresenter(gameController);
+        final GamePresenter gamePresenter = new GamePresenter(gameAnchorPane);
+        gameAnchorPane.setPresenter(gamePresenter);
 
         Scene scene = new Scene(gameAnchorPane, 1300, 800);
-        stage.setTitle("Hello!");
+        stage.setTitle("Urban Strategy");
         stage.setScene(scene);
         stage.show();
+
+        gamePresenter.play();
     }
 
     public static void main(String[] args) {
-        GamePresenter gameController = new GamePresenter(new GameView());
-        gameController.play();
         launch();
     }
 }
--- UrbanStrategy/src/main/java/module-info.java
+++ UrbanStrategy/src/main/java/module-info.java
@@ -2,16 +2,5 @@ module com.example.urbanstrategy {
     requires javafx.controls;
     requires javafx.fxml;
 
-
-//    opens com.example.urbanstrategy to javafx.fxml;
-
     exports com.example.urbanstrategy.application;
-//    exports com.example.urbanstrategy.ui;
-//    exports com.example.urbanstrategy.buildings;
-//    exports com.example.urbanstrategy.resources;
-//    exports com.example.urbanstrategy.transports;
-//    exports com.example.urbanstrategy.controllers;
-
-//    opens com.example.urbanstrategy.ui to javafx.fxml;
-//    opens com.example.urbanstrategy.controllers to javafx.fxml;
 }
